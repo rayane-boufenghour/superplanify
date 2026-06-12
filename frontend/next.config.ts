dde4d8a3bdf577719b2653a8697bdc5b4218f9d8
rayane-boufenghour <rayane.boufenghour@gmail.com>
2026-06-12 18:29:21 +0200

    Added headers in next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,8 +3,36 @@ import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin()
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=63072000; includeSubDomains; preload",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
+          },
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default withNextIntl(nextConfig)
